12e6863354124dc2f6bb1f7d9722c10490fd23da
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2019-01-13 00:55:10 +0100

    store clojure maps as clob and read all clobs as clojure maps

--- src/clj/pan_toksyczny/db/core.clj
+++ src/clj/pan_toksyczny/db/core.clj
@@ -17,29 +17,48 @@
   java.sql.Timestamp
   (result-set-read-column [v _2 _3]
     (.toLocalDateTime v))
+
   java.sql.Date
   (result-set-read-column [v _2 _3]
     (.toLocalDate v))
+
   java.sql.Time
   (result-set-read-column [v _2 _3]
-    (.toLocalTime v)))
+    (.toLocalTime v))
+
+  java.sql.Clob
+  (result-set-read-column [v _2 _3]
+    (let [s (slurp (.getCharacterStream v))]
+      (try
+        (read-string s)
+        (catch Exception e
+          s)))))
+
 
 (extend-protocol jdbc/ISQLValue
   java.util.Date
   (sql-value [v]
     (java.sql.Timestamp. (.getTime v)))
+
   java.time.LocalTime
   (sql-value [v]
     (jt/sql-time v))
+
   java.time.LocalDate
   (sql-value [v]
     (jt/sql-date v))
+
   java.time.LocalDateTime
   (sql-value [v]
     (jt/sql-timestamp v))
+
   java.time.ZonedDateTime
   (sql-value [v]
-    (jt/sql-timestamp v)))
+    (jt/sql-timestamp v))
+
+  clojure.lang.IPersistentMap
+  (sql-value [v]
+    (pr-str v)))
 
 
 (defn get-or-create-user!
